5705bbf99f769a1960bb511e908ae011c403d448
Tanner Dale <tanner@tannerdale.com>
2021-06-19 20:15:45 -0500

    Hard code board size

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,13 @@
 class Board
-  attr_reader :row_count, :column_count
   attr_accessor :board
 
   def initialize
-    @row_count = 6
-    @column_count = 7
     @board = create
   end
 
   def create
     the_board = []
-    @column_count.times do
+    6.times do
       the_board << %w(. . . . . . .)
     end
     the_board
