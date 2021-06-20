e571e04b75376a2ee4eb4f086d448d2e33a1ee0f
Tanner Dale <tanner@tannerdale.com>
2021-06-19 19:05:31 -0500

    Add start message

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -2,9 +2,16 @@ require './lib/board'
 
 class ConnectFour
   attr_reader :board
+
   def initialize
     @board = Board.new
   end
-end
 
-ConnectFour.new.board.show
+  def start
+    puts "Welcome to Connect Four!"
+    puts "You will be playing as 'X'. The computer will be 'O'."
+    puts "When prompted, select which column you would like to place your piece."
+    puts ""
+    @board.show
+  end
+end
